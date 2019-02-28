53c76da73a7b6c7472399d71a76304cc7e6e7972
vikashkumar <vikash423q@gmail.com>
2019-02-28 09:38:47 +0530

    basic

--- lib/main.dart
+++ lib/main.dart
@@ -1,111 +1,352 @@
 import 'package:flutter/material.dart';
 
-void main() => runApp(MyApp());
+void main() => runApp(MaterialApp(
+      title: 'So Lyrical',
+      home: Home(),
+      theme: ThemeData(primaryColor: Colors.red[400]),
+    ));
 
-class MyApp extends StatelessWidget {
-  // This widget is the root of your application.
+class Home extends StatefulWidget {
+  @override
+  State<StatefulWidget> createState() {
+    return _HomeState();
+  }
+}
+
+class _HomeState extends State<Home> {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(
-      title: 'Flutter Demo',
-      theme: ThemeData(
-        // This is the theme of your application.
-        //
-        // Try running your application with "flutter run". You'll see the
-        // application has a blue toolbar. Then, without quitting the app, try
-        // changing the primarySwatch below to Colors.green and then invoke
-        // "hot reload" (press "r" in the console where you ran "flutter run",
-        // or simply save your changes to "hot reload" in a Flutter IDE).
-        // Notice that the counter didn't reset back to zero; the application
-        // is not restarted.
-        primarySwatch: Colors.blue,
+    return Scaffold(
+      appBar: AppBar(
+        title: Text('So Lyrical'),
+        actions: <Widget>[
+          IconButton(
+            icon: Icon(Icons.search),
+            onPressed: () {
+              print('search button pressed');
+            },
+          ),
+        ],
+        // bottom: TabBar(
+        //   tabs: <Widget>[
+        //     SongListTab(),
+        //     LyricsTab(),
+        //   ],
+        //   controller: TabController(
+        //     initialIndex: 0,
+        //     length: 2,
+        //     vsync: this,
+        //   ),
+        // ),
       ),
-      home: MyHomePage(title: 'Flutter Demo Home Page'),
+      body: SongListTab(),
     );
   }
 }
 
-class MyHomePage extends StatefulWidget {
-  MyHomePage({Key key, this.title}) : super(key: key);
-
-  // This widget is the home page of your application. It is stateful, meaning
-  // that it has a State object (defined below) that contains fields that affect
-  // how it looks.
-
-  // This class is the configuration for the state. It holds the values (in this
-  // case the title) provided by the parent (in this case the App widget) and
-  // used by the build method of the State. Fields in a Widget subclass are
-  // always marked "final".
-
-  final String title;
-
-  @override
-  _MyHomePageState createState() => _MyHomePageState();
-}
-
-class _MyHomePageState extends State<MyHomePage> {
-  int _counter = 0;
-
-  void _incrementCounter() {
-    setState(() {
-      // This call to setState tells the Flutter framework that something has
-      // changed in this State, which causes it to rerun the build method below
-      // so that the display can reflect the updated values. If we changed
-      // _counter without calling setState(), then the build method would not be
-      // called again, and so nothing would appear to happen.
-      _counter++;
-    });
-  }
-
+class LyricsTab extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    // This method is rerun every time setState is called, for instance as done
-    // by the _incrementCounter method above.
-    //
-    // The Flutter framework has been optimized to make rerunning build methods
-    // fast, so that you can just rebuild anything that needs updating rather
-    // than having to individually change instances of widgets.
-    return Scaffold(
-      appBar: AppBar(
-        // Here we take the value from the MyHomePage object that was created by
-        // the App.build method, and use it to set our appbar title.
-        title: Text(widget.title),
+    return Center(
+      child: Container(
+        child: Text('Lyrics Page'),
       ),
-      body: Center(
-        // Center is a layout widget. It takes a single child and positions it
-        // in the middle of the parent.
-        child: Column(
-          // Column is also layout widget. It takes a list of children and
-          // arranges them vertically. By default, it sizes itself to fit its
-          // children horizontally, and tries to be as tall as its parent.
-          //
-          // Invoke "debug painting" (press "p" in the console, choose the
-          // "Toggle Debug Paint" action from the Flutter Inspector in Android
-          // Studio, or the "Toggle Debug Paint" command in Visual Studio Code)
-          // to see the wireframe for each widget.
-          //
-          // Column has various properties to control how it sizes itself and
-          // how it positions its children. Here we use mainAxisAlignment to
-          // center the children vertically; the main axis here is the vertical
-          // axis because Columns are vertical (the cross axis would be
-          // horizontal).
-          mainAxisAlignment: MainAxisAlignment.center,
-          children: <Widget>[
-            Text(
-              'You have pushed the button this many times:',
-            ),
-            Text(
-              '$_counter',
-              style: Theme.of(context).textTheme.display1,
-            ),
-          ],
+    );
+  }
+}
+
+class SongListTab extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return Column(
+      children: <Widget>[
+        Expanded(
+          child: ListView(
+            children: <Widget>[
+              ListTile(
+                leading: Image.network(
+                  'https://source.unsplash.com/50x50/random/?music',
+                  height: 50,
+                  width: 50,
+                ),
+                isThreeLine: false,
+                title: Text('Red'),
+                subtitle: Text('Taylor Swift'),
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 8.0, vertical: 0.0),
+              ),
+              Divider(
+                height: 0.0,
+              ),
+              ListTile(
+                leading: Image.network(
+                  'https://source.unsplash.com/50x50/random/?music',
+                  height: 50,
+                  width: 50,
+                ),
+                isThreeLine: false,
+                title: Text('Call It What You Want'),
+                subtitle: Text('Taylor Swift'),
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 8.0, vertical: 0.0),
+              ),
+              Divider(
+                height: 0.0,
+              ),
+              ListTile(
+                leading: Image.network(
+                  'https://source.unsplash.com/50x50/random/?music',
+                  height: 50,
+                  width: 50,
+                ),
+                isThreeLine: false,
+                title: Text('Red'),
+                subtitle: Text('Taylor Swift'),
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 8.0, vertical: 0.0),
+              ),
+              Divider(
+                height: 0.0,
+              ),
+              ListTile(
+                leading: Image.network(
+                  'https://source.unsplash.com/50x50/random/?music',
+                  height: 50,
+                  width: 50,
+                ),
+                isThreeLine: false,
+                title: Text('New Year\'s Day'),
+                subtitle: Text('Taylor Swift'),
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 8.0, vertical: 0.0),
+              ),
+              Divider(
+                height: 0.0,
+              ),
+              ListTile(
+                leading: Image.network(
+                  'https://source.unsplash.com/50x50/random/?music',
+                  height: 50,
+                  width: 50,
+                ),
+                title: Text('All Too Well'),
+                subtitle: Text('Taylor Swift'),
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 8.0, vertical: 0.0),
+              ),
+              Divider(
+                height: 0.0,
+              ),
+              ListTile(
+                leading: Image.network(
+                  'https://source.unsplash.com/50x50/random/?music',
+                  height: 50,
+                  width: 50,
+                ),
+                title: Text('Red'),
+                subtitle: Text('Taylor Swift'),
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 8.0, vertical: 0.0),
+              ),
+              Divider(
+                height: 0.0,
+              ),
+              ListTile(
+                leading: Image.network(
+                  'https://source.unsplash.com/50x50/random/?music',
+                  height: 50,
+                  width: 50,
+                ),
+                title: Text('Call It What You Want'),
+                subtitle: Text('Taylor Swift'),
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 8.0, vertical: 0.0),
+              ),
+              Divider(
+                height: 0.0,
+              ),
+              ListTile(
+                leading: Image.network(
+                  'https://source.unsplash.com/50x50/random/?music',
+                  height: 50,
+                  width: 50,
+                ),
+                title: Text('Red'),
+                subtitle: Text('Taylor Swift'),
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 8.0, vertical: 0.0),
+              ),
+              Divider(
+                height: 0.0,
+              ),
+              ListTile(
+                leading: Image.network(
+                  'https://source.unsplash.com/50x50/random/?music',
+                  height: 50,
+                  width: 50,
+                ),
+                title: Text('New Year\'s Day'),
+                subtitle: Text('Taylor Swift'),
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 8.0, vertical: 0.0),
+              ),
+              Divider(
+                height: 0.0,
+              ),
+              ListTile(
+                leading: Image.network(
+                  'https://source.unsplash.com/50x50/random/?music',
+                  height: 50,
+                  width: 50,
+                ),
+                title: Text('All Too Well'),
+                subtitle: Text('Taylor Swift'),
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 8.0, vertical: 0.0),
+              ),
+              Divider(
+                height: 0.0,
+              ),
+              ListTile(
+                leading: Image.network(
+                  'https://source.unsplash.com/50x50/random/?music',
+                  height: 50,
+                  width: 50,
+                ),
+                title: Text('Red'),
+                subtitle: Text('Taylor Swift'),
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 8.0, vertical: 0.0),
+              ),
+              Divider(
+                height: 0.0,
+              ),
+              ListTile(
+                leading: Image.network(
+                  'https://source.unsplash.com/50x50/random/?music',
+                  height: 50,
+                  width: 50,
+                ),
+                title: Text('Call It What You Want'),
+                subtitle: Text('Taylor Swift'),
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 8.0, vertical: 0.0),
+              ),
+              Divider(
+                height: 0.0,
+              ),
+              ListTile(
+                leading: Image.network(
+                  'https://source.unsplash.com/50x50/random/?music',
+                  height: 50,
+                  width: 50,
+                ),
+                title: Text('Red'),
+                subtitle: Text('Taylor Swift'),
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 8.0, vertical: 0.0),
+              ),
+              Divider(
+                height: 0.0,
+              ),
+              ListTile(
+                leading: Image.network(
+                  'https://source.unsplash.com/50x50/random/?music',
+                  height: 50,
+                  width: 50,
+                ),
+                title: Text('New Year\'s Day'),
+                subtitle: Text('Taylor Swift'),
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 8.0, vertical: 0.0),
+              ),
+              Divider(
+                height: 0.0,
+              ),
+              ListTile(
+                leading: Image.network(
+                  'https://source.unsplash.com/50x50/random/?music',
+                  height: 50,
+                  width: 50,
+                ),
+                title: Text('All Too Well'),
+                subtitle: Text('Taylor Swift'),
+                contentPadding:
+                    EdgeInsets.symmetric(horizontal: 8.0, vertical: 0.0),
+              ),
+            ],
+          ),
         ),
-      ),
-      floatingActionButton: FloatingActionButton(
-        onPressed: _incrementCounter,
-        tooltip: 'Increment',
-        child: Icon(Icons.add),
-      ), // This trailing comma makes auto-formatting nicer for build methods.
+        Container(
+          decoration:
+              BoxDecoration(color: Colors.red[400], boxShadow: <BoxShadow>[
+            BoxShadow(
+              offset: Offset.zero,
+              blurRadius: 2,
+            ),
+          ]),
+          margin: EdgeInsets.all(0.0),
+          child: Container(
+            child: Column(
+              children: <Widget>[
+                Slider(
+                  activeColor: Colors.white,
+                  inactiveColor: Colors.grey,
+                  value: 20.0,
+                  onChanged: (val) {
+                    print('slider value changed ${val}');
+                  },
+                  min: 0.0,
+                  max: 100.0,
+                ),
+                Row(
+                  mainAxisAlignment: MainAxisAlignment.spaceBetween,
+                  children: <Widget>[
+                    SizedBox(
+                      width: 8.0,
+                    ),
+                    Image.network(
+                      'https://source.unsplash.com/50x50/random/?music',
+                      height: 60,
+                      width: 60,
+                    ),
+                    SizedBox(
+                      width: 8.0,
+                    ),
+                    Text(
+                      'Call It What You Want',
+                      style: TextStyle(
+                          fontSize: 18.0, fontWeight: FontWeight.w300),
+                    ),
+                    SizedBox(
+                      width: 8.0,
+                    ),
+                    Row(
+                      children: <Widget>[
+                        IconButton(
+                          icon: Icon(Icons.play_arrow, size: 40.0),
+                          onPressed: () {
+                            print('play button pressed');
+                          },
+                        ),
+                        IconButton(
+                          icon: Icon(Icons.skip_next, size: 40.0),
+                          onPressed: () {
+                            print('next button pressed');
+                          },
+                        ),
+                      ],
+                    ),
+                    SizedBox(
+                      width: 8.0,
+                    ),
+                  ],
+                ),
+              ],
+            ),
+          ),
+        ),
+      ],
     );
   }
 }
